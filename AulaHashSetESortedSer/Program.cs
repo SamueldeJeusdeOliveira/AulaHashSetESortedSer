6fa1e68ed6324c711b79774dbfd1f86707943d6d
SamuelDeJesusDeOliveira <samuksjj@gmail.com>
2025-03-09 13:57:09 -0300

    Adicionar arquivos de projeto.

---
+++ AulaHashSetESortedSer/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
